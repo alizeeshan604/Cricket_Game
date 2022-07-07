4207e4e566337606646e35c3c5b90cd39b155afa
Sohaib Ali Khan <84563883+sohaib1268@users.noreply.github.com>
2022-07-07 18:11:05 +0500

    Ball bouncing and moving now

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Vector3 movementSpeed; //1
+    public Space space; //2
+
+    void Update()
+    {
+        transform.Translate(movementSpeed * Time.deltaTime, space);
+
+    }
+}
