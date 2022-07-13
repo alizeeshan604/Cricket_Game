d0bebaeb791008607df8ba60f6fcf55ceba6f772
Sohaib Ali Khan <84563883+sohaib1268@users.noreply.github.com>
2022-07-13 11:58:47 +0500

    fixing smooth damp

--- Assets/Scripts/movethebat.cs
+++ Assets/Scripts/movethebat.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class movethebat : MonoBehaviour
 {
-    public float moveSpeed = 10;
+    public float maxTurnSpeed = 90;
+    public float smoothTime = 0.3f;
+    float angle;
+    float currentVelocity;
     public Camera cam;
+
 
     void Update()
     {
-        Vector2 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = Vector2.MoveTowards(transform.position, mousePosition, moveSpeed * Time.deltaTime);
-        
+        Vector3 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePosition - transform.position;
+        float targetAngle = Vector2.SignedAngle(Vector2.right, direction);
+        angle = Mathf.SmoothDampAngle(angle, targetAngle, ref currentVelocity, smoothTime, maxTurnSpeed);
+        transform.eulerAngles = new Vector3(0, 0, angle);
     }
     
 }
