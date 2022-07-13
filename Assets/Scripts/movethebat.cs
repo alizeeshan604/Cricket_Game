91b4ae10630efcb99994afb60bb208d8ef668cd4
Sohaib Ali Khan <84563883+sohaib1268@users.noreply.github.com>
2022-07-13 11:52:07 +0500

    bat moving

--- Assets/Scripts/movethebat.cs
+++ Assets/Scripts/movethebat.cs
@@ -4,25 +4,14 @@ using UnityEngine;
 
 public class movethebat : MonoBehaviour
 {
-    
+    public float moveSpeed = 10;
+    public Camera cam;
 
-    // Update is called once per frame
-    [SerializeField]
-    Transform target;
-    float speed = 6f;
-    Vector2 targetPos; 
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            targetPos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            targetPos.y = 0; // I fixed Y value to zero you may change this to value you want
-            target.position = targetPos;
-            if ((Vector2)transform.position != targetPos)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-            }
-        }
-    }   
+        Vector2 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.MoveTowards(transform.position, mousePosition, moveSpeed * Time.deltaTime);
+        
+    }
     
 }
