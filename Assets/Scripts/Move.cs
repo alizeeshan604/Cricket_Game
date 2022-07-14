24eb836e100544315b1d6bdd6bb6d712f4114a07
alizeeshan604 <82883473+alizeeshan604@users.noreply.github.com>
2022-07-14 14:40:25 +0500

    random speed of balls

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -2,23 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static System.Random;
+//using System.random;
 
 public class Move : MonoBehaviour
 {
     public Vector3 movementSpeed; //1
     public Space space; //2
     private bool flag = true;
-
+   
+  
+    void Awake()
+    {
+        System.Random ran = new System.Random();
+        movementSpeed.x = ran.Next(-130, -50);
+    }
     void Update()
     {
-        if (flag == true)
-        {
+        
+           
             transform.Translate(movementSpeed * Time.deltaTime, space);
-        }
     }
     public void changeflag()
     {
-        flag = false;
+        flag = false; 
     }
     
     //public void OnCollisionEnter(Collision other)
